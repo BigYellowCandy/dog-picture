4fd24d06369a561f0c811db8505611887f1e0838
Hyeonu_Lee <hyeonulee@gmail.com>
2023-07-22 01:56:44 -0700

    new commit

---
+++ intro.js
@@ -0,0 +1,19 @@
+/*
+    Hyeonu
+    Learning about JavaScript
+
+*/
+
+//variables
+var first = 0;
+var str = "Dion loves big dicks down his tight throat"
+var bool = true;
+var First = 0;
+
+var num;
+
+console.log(str); 
+
+first = 3;
+
+const PI = 3.14;
